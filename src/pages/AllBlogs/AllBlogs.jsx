9e3a085fba5f29608b42619fa8298e84db0a2f22
maheshbasnet089 <basnetmanish089@gmail.com>
2023-08-29 22:01:22 +0545

    add getBlogs

--- src/pages/AllBlogs/AllBlogs.jsx
+++ src/pages/AllBlogs/AllBlogs.jsx
@@ -1,11 +1,55 @@
+import { useEffect, useState } from "react"
 import Navbar from "../../components/Navbar/Navbar"
+import './AllBlogs.css'
+import axios from "axios"
 
 
 const AllBlogs = () => {
+  const [blogs,setBlogs] = useState([])
+
+
+  const fetchBlogs = async()=>{
+    const response = await axios.get("https://64ee09451f8721827142370c.mockapi.io/blogs/")
+    if(response.status == 200){
+      console.log(response.data)
+
+    setBlogs(response.data)
+  
+    
+  }
+
+
+  }
+
+  useEffect(()=>{
+    fetchBlogs()
+   
+  },[])
+
+
   return (
     <div>
         <Navbar />
-        AllBlogs</div>
+        
+<div style={{display:'flex',justifyContent:"space-evenly",flexWrap:'wrap'}}>
+{
+  blogs.map((blog)=>{
+    return (
+      <div className="card" key={blog.id}>
+     <img src={blog.avatar} alt="Avatar"  width='100%' />
+      <div className="container">
+        <h4><b>{blog.title}</b></h4> 
+        <p style={{color:"red"}}>{blog.description}</p> 
+        <p>{blog.createdAt}</p>
+      </div>
+</div>
+    )
+  })
+}
+
+</div>
+
+        </div>
   )
 }
 
---
+++ test.js
@@ -0,0 +1,32 @@
+const response = [
+
+{
+    title : "Manish",
+    description : " I am a student"
+},
+{
+    title : "Mahesh",
+    description : " I am a student"
+},
+{
+    title : "Pranjal",
+    description : " I am a student"
+}
+]
+
+
+// forEach
+
+// response.forEach((res)=>{
+//     console.log(res)
+// })
+
+// map
+// response.map((res)=>{
+//     console.log(res)
+// })
+
+// for loop
+// for(var i = 0 ; i < response.length; i++){
+//     console.log(response[i].title,i)
+// }
