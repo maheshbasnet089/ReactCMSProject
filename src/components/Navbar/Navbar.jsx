6ec97831d156bb48a62edaf2a3594ffafcd89221
maheshbasnet089 <basnetmanish089@gmail.com>
2023-09-02 20:21:58 +0545

    add test

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,6 +6,8 @@ const Navbar = () => {
     <ul className="nav-list">
         <li><a href="/">Home</a></li>
         <li><a href="/createBlog">Add Blog</a></li>
+        <li><a href="/">Test</a></li>
+
       
     </ul>
 </nav>
